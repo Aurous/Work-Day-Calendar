4bb6f6e938b07ffcf147e252c5a9f88813765b63
Ryan Hill <ryan@ryanhill.com>
2025-06-03 18:57:12 -0500

    few adjustments, need to reformat to clean up

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,10 +1,49 @@
 <script setup lang="ts">
     import { DateTime } from 'luxon';
-    // import { parseDate } from '@internationalized/date'
 
     const date = ref(DateTime.now());
 
-    // const calendarDate = ref(parseDate(DateTime.now().toFormat('yyyy-LL-dd')));
+    const events = [
+        {
+            id: 1,
+            title: 'Meeting with Team',
+            start: '2025-06-03T09:30:00',
+            end: '2025-06-03T10:30:00'
+        },
+        {
+            id: 2,
+            title: 'Lunch Break',
+            start: '2025-06-03T12:00:00',
+            end: '2025-06-03T13:00:00'
+        },
+        {
+            id: 3,
+            title: 'Design Review',
+            start: '2025-06-03T15:15:00',
+            end: '2025-06-03T16:00:00'
+        }
+    ];
+
+    function getEventStyle(event) {
+        const start = new Date(event.start);
+        const end = new Date(event.end);
+
+        const startHour = start.getHours() + start.getMinutes() / 60;
+        const endHour = end.getHours() + end.getMinutes() / 60;
+        const duration = endHour - startHour;
+
+        const top = (startHour / 24) * 100;
+        const height = (duration / 24) * 100;
+
+        return {
+            top: `${top}%`,
+            height: `${height}%`
+        };
+    }
+
+    function formatTime(datetime) {
+        return DateTime.fromISO(datetime);
+    }
 </script>
 
 <template>
@@ -25,23 +64,24 @@
                 to do list
             </div>
         </div>
-        <div class="h-fill">
-            <div class="relative h-full border rounded-lg overflow-hidden">
-                <div class="absolute left-0 top-0 w-16 h-full border-r">
-                    <div v-for="hour in 24" :key="hour" class="h-[calc(100%/24)] border-b text-xs text-right pr-2 py-1">
-                        {{ (hour - 1 + 24) % 24 }}:00
-                    </div>
+        <div class="relative h-full overflow-hidden">
+            <div class="absolute left-0 top-0 w-16 h-full border-r">
+                <div v-for="hour in 24" :key="hour" class="h-[calc(100%/24)] border-b bg-white text-black text-xs text-right pr-2 py-1">
+                    {{ (hour - 1 + 24) % 24 }}:00
                 </div>
-                <div class="ml-16 h-full relative">
-                    <div v-for="hour in 24" :key="hour" class="h-[calc(100%/24)] border-b"></div>
-
-                    <div v-for="event in events" :key="event.id" class="absolute left-0 w-full px-2"
-                        :style="getEventStyle(event)">
-                        <div class="bg-blue-500 text-white text-sm rounded p-2 shadow">
-                            <div class="font-semibold">{{ event.title }}</div>
-                            <div class="text-xs">
-                                {{ formatTime(event.start) }} - {{ formatTime(event.end) }}
-                            </div>
+            </div>
+            <div class="ml-16 h-full relative">
+                <div v-for="hour in 24" :key="hour" class="h-[calc(100%/24)] border-b bg-white" />
+                <div 
+                    v-for="event in events" 
+                    :key="event.id" 
+                    class="absolute left-0 w-full px-2"
+                    :style="getEventStyle(event)
+                ">
+                    <div class="bg-blue-500 text-white text-sm rounded p-2 shadow">
+                        <div class="font-semibold">{{ event.title }}</div>
+                        <div class="text-xs">
+                            {{ formatTime(event.start).toFormat('h:mma') }} - {{ formatTime(event.end).toFormat('hh:mma') }}
                         </div>
                     </div>
                 </div>
