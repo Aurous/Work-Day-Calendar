9e85d2bd106f81babe9f4f684b6de1e76c5daf73
Ryan Hill <ryan@ryanhill.com>
2025-06-03 09:09:45 -0500

    the kid moved, but took a dump

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -8,19 +8,21 @@ const calendarDate = ref(parseDate(DateTime.now().toFormat('yyyy-LL-dd')));
 </script>
 
 <template>
-    <div class="h-dvh w-full grid grid-cols-3 grid-rows-12 gap-4">
-        <div class="row-span-12 col-span-2">
-            <h3 class="bg-white text-black text-center">{{ date }}</h3>
-        </div>
-        <div class="row-span-12">
-            day view
-        </div>
-        <div class="col-span-2 grid grid-cols-2 gap-4">
-            <div class="">
-                to do list
+    <div class="h-dvh w-full grid grid-cols-3 gap-4">
+        <div class="col-span-2">
+            <div class="h-fill w-full grid grid-cols-2 gap-4">
+                <h3 class="col-span-2 bg-white text-black text-center">{{ date }}</h3>
+                    <div class="col-span-1 ">
+                        to do list
+                    </div>
+                    <div class="col-span-1">
+                        <UCalendar v-model="calendarDate" />
+                    </div>
             </div>
-            <div class="">
-                <UCalendar v-model="calendarDate" />
+        </div>
+        <div class="col-span-1">
+            <div>
+                day view
             </div>
         </div>
     </div>
