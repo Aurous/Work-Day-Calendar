04e15b3a59c72e38e81e90d8e414f7b73bdec236
Ryan Hill <ryan@ryanhill.com>
2025-06-07 22:26:46 -0500

    press process pleep

--- components/table/view.vue
+++ components/table/view.vue
@@ -1,128 +1,84 @@
 <script setup lang="ts">
-	import type { TableColumn } from '@nuxt/ui';
 	// const props = defineProps<{
+	//  columns: string[];
 	// 	items: [];
 	// }>();
 
-	const UBadge = resolveComponent('UBadge');
-	const UButton = resolveComponent('UButton');
-
-	type Payment = {
-		id: string;
-		date: string;
-		status: 'paid' | 'failed' | 'refunded';
-		email: string;
-		amount: number;
-	};
-
-	const data = ref<Payment[]>([
+	const items = [
 		{
-			id: '4600',
-			date: '2024-03-11T15:30:00',
-			status: 'paid',
-			email: 'james.anderson@example.com',
-			amount: 594,
+			id: 1,
+			name: 'Google Calendar',
+			color: 'blue',
 		},
 		{
-			id: '4599',
-			date: '2024-03-11T10:10:00',
-			status: 'failed',
-			email: 'mia.white@example.com',
-			amount: 276,
+			id: 2,
+			name: 'Teams Calendar',
+			color: 'green',
 		},
 		{
-			id: '4598',
-			date: '2024-03-11T08:50:00',
-			status: 'refunded',
-			email: 'william.brown@example.com',
-			amount: 315,
+			id: 3,
+			name: 'Yahoo Calendar',
+			color: 'yellow',
 		},
 		{
-			id: '4597',
-			date: '2024-03-10T19:45:00',
-			status: 'paid',
-			email: 'emma.davis@example.com',
-			amount: 529,
-		},
-		{
-			id: '4596',
-			date: '2024-03-10T15:55:00',
-			status: 'paid',
-			email: 'ethan.harris@example.com',
-			amount: 639,
-		},
-	]);
-
-	const columns: TableColumn<Payment>[] = [
-		{
-			accessorKey: 'id',
-			header: '#',
-			cell: ({ row }) => `#${row.getValue('id')}`,
-		},
-		{
-			accessorKey: 'date',
-			header: 'Date',
-			cell: ({ row }) => {
-				return new Date(row.getValue('date')).toLocaleString('en-US', {
-					day: 'numeric',
-					month: 'short',
-					hour: '2-digit',
-					minute: '2-digit',
-					hour12: false,
-				});
-			},
-		},
-		{
-			accessorKey: 'status',
-			header: 'Status',
-			cell: ({ row }) => {
-				const color = {
-					paid: 'success' as const,
-					failed: 'error' as const,
-					refunded: 'neutral' as const,
-				}[row.getValue('status') as string];
-
-				return h(
-					UBadge,
-					{ class: 'capitalize', variant: 'subtle', color },
-					() => row.getValue('status')
-				);
-			},
-		},
-		{
-			accessorKey: 'email',
-			header: ({ column }) => {
-				const isSorted = column.getIsSorted();
-
-				return h(UButton, {
-					color: 'neutral',
-					variant: 'ghost',
-					label: 'Email',
-					icon: isSorted
-						? isSorted === 'asc'
-							? 'i-lucide-arrow-up-narrow-wide'
-							: 'i-lucide-arrow-down-wide-narrow'
-						: 'i-lucide-arrow-up-down',
-					class: '-mx-2.5',
-					onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-				});
-			},
-		},
-		{
-			accessorKey: 'amount',
-			header: () => h('div', { class: 'text-right' }, 'Amount'),
-			cell: ({ row }) => {
-				const amount = Number.parseFloat(row.getValue('amount'));
-
-				const formatted = new Intl.NumberFormat('en-US', {
-					style: 'currency',
-					currency: 'EUR',
-				}).format(amount);
-
-				return h('div', { class: 'text-right font-medium' }, formatted);
-			},
+			id: 3,
+			name: 'Proton Calendar',
+			color: 'red',
 		},
 	];
+
+	const keys = ['id', 'name', 'color'];
+
+	function headerSort(key) {
+		const header = key
+			.split(' ')
+			.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+			.join(' ');
+		return {
+			accessorKey: key,
+			header,
+		};
+	}
+
+	const columns = keys.map(headerSort);
+
+	// const columns: TableColumn<any>[] = [
+	// 	{
+	// 		accessorKey: 'id',
+	// 		header: 'Id',
+	// 	},
+	// 	{
+	// 		accessorKey: 'date',
+	// 		header: 'Date',
+	// 	},
+	// 	{
+	// 		accessorKey: 'status',
+	// 		header: 'Status',
+	// 	},
+	// 	{
+	// 		accessorKey: 'email',
+	// 		header: ({ column }) => {
+	// 			const isSorted = column.getIsSorted();
+
+	// 			return h(UButton, {
+	// 				color: 'neutral',
+	// 				variant: 'ghost',
+	// 				label: 'Email',
+	// 				icon: isSorted
+	// 					? isSorted === 'asc'
+	// 						? 'i-lucide-arrow-up-narrow-wide'
+	// 						: 'i-lucide-arrow-down-wide-narrow'
+	// 					: 'i-lucide-arrow-up-down',
+	// 				class: '-mx-2.5',
+	// 				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+	// 			});
+	// 		},
+	// 	},
+	// 	{
+	// 		accessorKey: 'amount',
+	// 		header: 'Amount',
+	// 	},
+	// ];
 
 	const sorting = ref([
 		{
@@ -135,7 +91,7 @@
 <template>
 	<UTable
 		v-model:sorting="sorting"
-		:data="data"
+		:data="items"
 		:columns="columns"
 		class="flex-1"
 	/>
