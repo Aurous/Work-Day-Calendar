1b408f389a2362714b346838982bf057bb31b839
Ryan Hill <ryan@ryanhill.com>
2025-06-09 10:55:11 -0500

    overhaul to table view

--- components/table/view.vue
+++ components/table/view.vue
@@ -4,36 +4,29 @@
 		api: string;
 	}>();
 
-	const isMounted = ref(false);
-	const columns = ref(
-		headers.map(({ header, accessorKey, sortable }) => {
-			return {
-				accessorKey,
-				header: ({ column }) => {
-					const isSorted = column.getIsSorted();
+	const getSortIcon = (isSorted) =>
+		isSorted
+			? isSorted === 'asc'
+				? 'i-lucide-arrow-up-narrow-wide'
+				: 'i-lucide-arrow-down-wide-narrow'
+			: 'i-lucide-arrow-up-down';
+	const createSortableHeader =
+		(header) =>
+		({ column }) =>
+			h(resolveComponent('UButton'), {
+				color: 'neutral',
+				variant: 'ghost',
+				label: header,
+				icon: getSortIcon(column.getIsSorted()),
+				class: '-mx-2.5',
+				onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+			});
+	const addSortableHeader = ({ header, accessorKey, sortable }) => ({
+		accessorKey,
+		header: sortable ? createSortableHeader(header) : header,
+	});
+	const columns = ref(headers.map(addSortableHeader));
 
-					if (!sortable) return header;
-
-					let icon = 'i-lucide-arrow-up-down';
-					const asc = 'i-lucide-arrow-up-narrow-wide';
-					const desc = 'i-lucide-arrow-down-wide-narrow';
-					if (isSorted === 'asc') icon = asc;
-					else if (isSorted === 'desc') icon = desc;
-
-					return h(resolveComponent('UButton'), {
-						color: 'neutral',
-						variant: 'ghost',
-						label: header,
-						icon,
-						class: '-mx-2.5',
-						onClick: () => {
-							column.toggleSorting(column.getIsSorted() === 'asc');
-						},
-					});
-				},
-			};
-		})
-	);
 	const query = ref({
 		currentPage: 1,
 		perPage: 5,
@@ -46,46 +39,51 @@
 			desc: false,
 		},
 	]);
-
-	const { data, error, execute } = await useFetch(() => `/api/${api}`, {
-		query: query.value,
-		immediate: true,
-		onResponse({ request, response, options }) {
-			// Process the response data
-			console.log('token', response._data);
-		},
-	});
-
+	// api call for table
+	const { data, error, pending, execute } = await useFetch(
+		() => `/api/${api}`,
+		{
+			query: query.value,
+			immediate: true,
+		}
+	);
+	// run the data execute when the query has changed
 	watch(
 		query,
 		() => {
-			console.log('asdf');
 			execute();
 		},
 		{ deep: true }
 	);
-
+	// make sure to update the query when changing the sort
 	watch(sorting, ([{ id = 'id', desc = false }]) => {
-		console.log(id, desc);
-		console.log(desc ? 'desc' : 'asc');
 		query.value.orderBy = id;
 		query.value.order = desc ? 'desc' : 'asc';
-		// if (val.length > 0) {
-		// 	query.value.orderBy = val[0].id;
-		// 	query.value.order = val[0].desc ? 'desc' : 'asc';
-		// }
 	});
+	// prevent data from becoming null when fetching updates
+	watch(
+		data,
+		(newData, oldData, callback) => {
+			if (newData === null) data.value = oldData;
+		},
+		{ deep: true }
+	);
 </script>
 
+<!-- 
+	TODO: figure out a better way for sorting
+	PROBLEM: slight show of data sorting by table for api returns data
+	might be able to use query over sort ref
+ -->
 <template>
-	<div class="w-full space-y-4 pb-4">
+	<div class="grid h-full grid-rows-2">
 		<UTable
 			ref="table"
 			v-model:pagination="query"
 			v-model:sorting="sorting"
 			:data="data?.data"
 			:columns="columns"
-			class="flex-1"
+			:loading="pending"
 		/>
 		<UPagination
 			v-model:page="query.currentPage"
@@ -93,7 +91,15 @@
 			:items-per-page="query.perPage"
 			show-first
 			show-last
-			class="justify-end"
 		/>
 	</div>
+	<!-- 
+		TODO: ensure that spacing does not change on table and pagination
+		probably need to use flex, but grid might work better
+	-->
+	<!-- 
+		TODO: add perPage component
+		might use dropdown?
+		slider? => probably not 
+	-->
 </template>
