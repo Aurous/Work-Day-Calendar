8f9daf8b57601b82c1e30dff8b6fa9d26b8cdfbf
Ryan Hill <ryan@ryanhill.com>
2025-06-13 17:32:16 -0500

    pause scroll for a second and prepare for socket updates

--- components/todo/view.vue
+++ components/todo/view.vue
@@ -22,20 +22,36 @@
 
 	const activeTab = ref('incomplete');
 	const progress = ref(0);
+	const pauseCount = ref(0);
 	let animation: NodeJS.Timer;
 
 	onMounted(() => {
 		animation = setInterval(() => {
-			progress.value += 1;
-			if (progress.value >= 100) {
-				progress.value = 0;
-				const currentIndex = items.value.findIndex(
-					({ value }) => value === activeTab.value
-				);
-				const nextIndex = (currentIndex + 1) % items.value.length;
-				activeTab.value = items.value[nextIndex].label;
+			if (pauseCount.value > 0) {
+				pauseCount.value -= 1;
+			} else {
+				progress.value += 1;
+				if (progress.value >= 100) {
+					progress.value = 0;
+					const currentIndex = items.value.findIndex(
+						({ value }) => value === activeTab.value
+					);
+					const nextIndex = (currentIndex + 1) % items.value.length;
+					activeTab.value = items.value[nextIndex].label;
+				}
 			}
 		}, 100);
+
+		const { on } = useSocket();
+		on('todo-remove', () => {
+			console.log('todo-remove');
+		});
+		on('todo-add', () => {
+			console.log('todo-add');
+		});
+		on('todo-edit', () => {
+			console.log('todo-edit');
+		});
 	});
 
 	onBeforeUnmount(() => {
@@ -53,6 +69,7 @@
 				trigger: 'grow',
 			}"
 			class="gap-0.25"
+			@click="pauseCount = 25"
 		/>
 		<UProgress v-model="progress" size="xs" />
 		<div>
