7710385418318774983ed15a3824cc3ef92254e0
Ryan Hill <ryan@ryanhill.com>
2025-06-13 19:25:12 -0500

    that was not needed

--- server/api/events/today.get.ts
+++ server/api/events/today.get.ts
@@ -1,13 +1,9 @@
-import type { Knex } from 'knex';
-
 export default defineEventHandler(async (event) => {
 	try {
 		return event.context
 			.knex('event')
 			.select('*')
-			.where((qb: Knex.QueryBuilder) => {
-				qb.whereRaw("date(startDT, 'localtime') = date('now',  'localtime')");
-			});
+			.whereRaw("date(startDT, 'localtime') = date('now',  'localtime')");
 	} catch (error) {
 		throw createError({
 			statusCode: 404,
