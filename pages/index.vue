6e1ef5655ab199a47a030f32470321916fe01de1
Ryan Hill <ryan@ryanhill.com>
2025-06-15 08:24:19 -0500

    make the display more friendly

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="grid h-dvh w-full grid-cols-5 grid-rows-5">
-		<CalendarDate class="col-span-2 col-start-1 row-start-1" />
-		<TodoView class="col-span-2 col-start-1 row-span-4 row-start-2" />
+	<div
+		class="grid h-dvh w-full grid-cols-1 grid-rows-[auto_1fr_auto] gap-2 p-2 md:grid-cols-5 md:grid-rows-5"
+	>
+		<CalendarDate class="md:col-span-2 md:col-start-1 md:row-start-1" />
+		<TodoView
+			class="md:col-span-2 md:col-start-1 md:row-span-4 md:row-start-2"
+		/>
 		<CalendarSchedule
-			class="col-span-3 col-start-3 row-span-5 row-start-1 border-l"
+			class="border-t md:col-span-3 md:col-start-3 md:row-span-5 md:row-start-1 md:border-t-0 md:border-l"
 		/>
 	</div>
 </template>
