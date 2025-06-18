fdd6c995ddf9dd112b82e082742ba97c9e228fae
Ryan Hill <ryan@ryanhill.com>
2025-06-18 00:41:01 -0500

    start working on saving data

--- pages/tasks/new.vue
+++ pages/tasks/new.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import type { FormSubmitEvent } from '@nuxt/ui';
+	import * as z from 'zod';
+
+	const schema = z.object({
+		title: z.string(),
+		priority: z.string(),
+	});
+
+	type Schema = z.output<typeof schema>;
+
+	const state = reactive<Partial<Schema>>({
+		title: undefined,
+		priority: undefined,
+	});
+
+	const toast = useToast();
+	async function onSubmit(event: FormSubmitEvent<Schema>) {
+		console.log(event.data);
+	}
+</script>
 
 <template>
-	<div>asdf</div>
+	<UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+		<UFormField label="Title" name="title">
+			<UInput v-model="state.title" />
+		</UFormField>
+
+		<UFormField label="Priority" name="priority">
+			<UInput v-model="state.priority" />
+		</UFormField>
+
+		<UButton type="submit">Save</UButton>
+	</UForm>
 </template>
